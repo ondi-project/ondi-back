dcd64613b23277d54102e6de8724009cf09b427e
Byeonggon Lee <gonny952@gmail.com>
2021-02-06 02:01:07 +0900

    Fix media

--- ondi/ondi/urls.py
+++ ondi/ondi/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +25,4 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/register/', include('dj_rest_auth.registration.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
