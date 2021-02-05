56895278a26533048f438b87a4e24c21c9058fcc
hyewon <khhw2126@gmail.com>
2021-02-05 14:49:40 +0900

    #4. p_viewcount추가

--- ondi/main/models.py
+++ ondi/main/models.py
@@ -11,18 +11,19 @@ class Product(models.Model):
     p_price = models.IntegerField(verbose_name="상품가격", default=0)
     #상품설명 어떻게오느냐따라달라질듯 RichTextUploadingField
     p_content = models.CharField(max_length=32, verbose_name="상품설명")
-    p_image = models.ImageField(verbose_name="대표사진", null=True, upload_to="")
+    p_image = models.ImageField(verbose_name="대표사진", upload_to="")
     p_tag = models.CharField(max_length=200)
     p_nego = models.BooleanField()
     p_date = models.DateTimeField(default=timezone.now, verbose_name="등록날짜")
     p_likecount = models.IntegerField(verbose_name="좋아요수", null=True, default=0)
     p_seller = models.ForeignKey(User , on_delete=models.CASCADE,default="")
     #라이브방송여부
-    p_live = models.CharField(max_length=200, verbose_name="라이브방송여부",default="") #['예정','진행중', '0'] #라이브진행중, 라이브종료,
-
+    p_live = models.CharField(max_length=200, verbose_name="라이브방송여부",default=0) #['예정','진행중', '0'] #라이브진행중, 라이브종료,
+    p_viewcount = models.IntegerField(verbose_name="조회수", null=True, default=0)
 class LiveProduct(models.Model):
     #일단 상품이아닌 라이브 깜짝방송 이런느낌으로#
     # {상품정보,라이브시간}
     #date확인다시!
     l_date = models.DateTimeField(default=timezone.now, verbose_name="live시작시간")
     l_product = models.ForeignKey(Product, on_delete=models.CASCADE,default="")
+    l_sprice = models.IntegerField(verbose_name="라이브 시작가격", default=0)
